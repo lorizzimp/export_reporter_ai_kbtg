26e587bdcbb1669356d88d36d0c87d30e8d5a244
Loriz.Kiehiz <loriz.kiehiz@gmail.com>
2021-03-17 22:26:25 +0700

    add project

---
+++ src/main/java/com/evobox/reportexporter/ReportexporterApplication.java
@@ -0,0 +1,13 @@
+package com.evobox.reportexporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportexporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportexporterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evobox/reportexporter/ReportexporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evobox.reportexporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportexporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
